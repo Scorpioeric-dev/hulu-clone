b1ae495ecb4b7f0badb413c4569e8c0d4da2f0d1
Scorpioeric-dev <ericscorpio1@gmail.com>
2021-01-13 20:18:40 -0800

    react-truncate

--- src/Components/VideoCard.js
+++ src/Components/VideoCard.js
@@ -1,24 +1,29 @@
 import React, { forwardRef } from "react";
 import "../CSS/VideoCard.css";
 import StarRate from "@material-ui/icons/StarRate";
-// import TextTruncate from 'react-text-truncate'
+import TextTruncate from "react-text-truncate";
 
 const base_url = "https://image.tmdb.org/t/p/original/";
 
-export const VideoCard = forwardRef(({ movie },ref) => {
+export const VideoCard = forwardRef(({ movie }, ref) => {
   return (
     <div ref={ref} className="videoCard">
       <img
         src={`${base_url}${movie.backdrop_path || movie.poster_path} `}
         alt=" movie poster"
       />
-      <p>{movie.overview}</p>
+      <TextTruncate
+        line={1}
+        element="p"
+        truncateText="..."
+        text={movie.overview}
+      />
+
       <h2>{movie.title || movie.original_name}</h2>
       <p className="videoCard_stats">
-      {movie.media_type && ` ${movie.media_type} . `}
-      {movie.release_date || movie.first_air_date} .
-         <StarRate/>{" "}
-        {movie.vote_count}
+        {movie.media_type && ` ${movie.media_type} . `}
+        {movie.release_date || movie.first_air_date} .
+        <StarRate /> {movie.vote_count}
       </p>
     </div>
   );
